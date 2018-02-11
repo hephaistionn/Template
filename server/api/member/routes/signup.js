b5b4395c45e6c56a0300e0e0e5c75ff0a874f353
hephaistionn <alexandre.monge.mail@gmail.com>
2018-02-11 15:01:45 +0100

    signup mail

--- server/api/member/routes/signup.js
+++ server/api/member/routes/signup.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const Member = require('../model');
+const sendmail = require('../../../services/mailjet');
 const mongoose = require('mongoose');
+const conf = require('../../../conf');
 
 function* signup(req, res) {
     if (req.body.email && req.body.username && req.body.password) {
@@ -14,6 +16,28 @@ function* signup(req, res) {
             level: 0
         });
         intance.owner = intance._id;
+
+        const emailData = {
+            Messages: [
+                {
+                    From: {
+                        Email: `noreply@${conf.APP_NAME}.com`,
+                        Name: conf.APP_NAME
+                    },
+                    To: [{
+                        Email: req.body.email,
+                        Name: req.body.username
+                    }],
+                    Subject: "Signed up successfully",
+                    TextPart: `Welcome to ${conf.APP_NAME}.\n Hey ${intance.username},\n Thanks for registering, 
+                    Use this code to validate your account.\n ${intance.token}`,
+                    HTMLPart: `<h1>Welcome to ${conf.APP_NAME}</h1><p>Hey ${intance.username},</p><p>Thanks for registering, 
+                    Use this code to validate your account.</p> <h2>${intance.token}</h2> `
+                }
+            ]
+        }
+        yield sendmail.request(emailData);
+
         yield intance.save();
 
         res.send('success');
--- server/conf.js
+++ server/conf.js
@@ -1,6 +1,7 @@
 module.exports = {
+    APP_NAME: 'ublocs',
     NODE_ENV: process.env.NODE_ENV,
-    HOST_NAME: process.env.NODE_ENV,
+    HOST_NAME: process.env.HOST_NAME,
     URI: process.env.URI,
     EMAIL_KEY: process.env.EMAIL_KEY,
     EMAIL_SECRET: process.env.EMAIL_SECRET,
